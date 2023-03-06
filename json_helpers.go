c4074bdebb99fcec3a83b0408e5e7382e37b654a
Casper Mogensen <casper.mogensen@gmail.com>
2023-03-06 20:20:48 +0100

    Change info print to debug for PostJson and add similar debug message for GetJson

--- json_helpers.go
+++ json_helpers.go
@@ -11,6 +11,7 @@ func (sh *SimpleHttp) GetJson(url string, dst interface{}) error {
 		return err
 	}
 
+	sh.logger.Debugf("GetJson Response: %s\n", data)
 	if err := json.Unmarshal([]byte(data), dst); err != nil {
 		return err
 	}
@@ -26,7 +27,7 @@ func (sh *SimpleHttp) PostJson(url string, dst interface{}, parseResponse bool) 
 		return err
 	}
 
-	sh.logger.Printf("PostJson Response: %s\n", data)
+	sh.logger.Debugf("PostJson Response: %s\n", data)
 	if parseResponse {
 		if err := json.Unmarshal([]byte(data), dst); err != nil {
 			return err
